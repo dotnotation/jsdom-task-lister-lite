36f0b40641a1adc58574c4aed5c19bc211391340
dotnotation <dotinmotionproductions@gmail.com>
2021-11-04 12:40:09 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let editMode = false;
+
 document.addEventListener("DOMContentLoaded", () => {
   const taskForm = document.getElementById('create-task-form');
   // grab form
@@ -12,8 +14,20 @@ function handleSubmit(event) {
   console.log(event);
   const taskInput = document.getElementById('new-task-description');
   // grab input field
-  addTask(taskInput.value);
+
+  if (!!editMode) {
+    editMode.children[0].innerText = taskInput.value
+    // edit the children of the individual task and the first child is the text
+    document.getElementById('submit-task').value = "Create New Task";
+    // resetting the submit button to say Create New Task again
+    editMode = false;
+    // ending edit mode
+  } else {
+    addTask(taskInput.value);
   // add the value of the input to the task list
+  }
+  taskInput.value = "";
+  // resetting the input field text
 }
 
 function addTask(task) {
@@ -23,13 +37,44 @@ function addTask(task) {
   // create a new div for each task
   const taskSpan = document.createElement('span');
   // create a span for each task
+  const deleteIcon = document.createElement('span');
+  // create 'x' element for deletion 
+  const editButton = document.createElement('button');
+  // create edit button
 
+  deleteIcon.innerHTML = "&#10006;";
+  // making delete icon look like an x
   taskSpan.innerText = task;
   // add the text to the task span
+  editButton.innerText = "Edit";
 
-  taskDiv.append(taskSpan);
-  // add the span to the div
+  deleteIcon.addEventListener("click", (event) => {
+    event.target.parentElement.remove();
+    //grabbing the parent element of the delete icon (taskDiv) and deleting it
+  })
+
+  editButton.addEventListener("click", handleEdit);
+
+  taskDiv.append(taskSpan, deleteIcon, editButton);
+  // add the task span, delete icon, and edit button to the individual task div
   tasksDiv.append(taskDiv);
   // add the individual task divs to the div container
 
+}
+
+function handleEdit(event) {
+  const taskDiv = event.target.parentElement;
+  // grabbing the parent to the individual task div
+  let task = taskDiv.children[0].innerText;
+  // grab the individual task div
+  const submitButton = document.getElementById('submit-task');
+  // grab submit button
+  const input = document.getElementById('new-task-description');
+  // get the text input
+  input.value = task;
+  // change the value of the text input to the comment we are editing
+  submitButton.value = "Edit";
+  // change the submit button to say Edit
+  editMode = taskDiv;
+  // edit the individual task div
 }
